1160a317f089e4ed0bcd87a4d77d5e985d8f9555
Aditi Pawaskar <aditipawaskar97@gmail.com>
2018-02-18 03:17:48 +0000

    Basic int data transfer BT SoftwareSerial UNTESTED

--- arduinoCode.ino
+++ arduinoCode.ino
@@ -1,12 +1,41 @@
+/*
+* Adding code for data transfer using Bluetooth,currently only Integer transfer
+*/
 
+#include<SoftwareSerial.h>
 
+#define rx 10
+#define tx 11
+
+SoftwareSerial BT(rx,tx); //RX,TX
+
+void incomingBTdata();
 
 void setup() {
-  // put your setup code here, to run once:
-
+ Serial.begin(9600);
+ BT.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
+}
+
+void incomingBTdata(){
+  while(!BT.available());
+  char a;
+  a = BT.read();
+    switch (a)
+    {
+       case '1': // put main code here
+                break;
+       case '2': // put main code here
+                break;
+      case '3':  // put main code here
+                break;
+      case '4': // put main code here
+               break;
+    }
+    delay(10);
+  }
 }
